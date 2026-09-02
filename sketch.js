5997ce2e2e0f3224282a2f58781c23bc8699d645
Alice <187536220+alicwyz@users.noreply.github.com>
2026-09-01 21:16:26 -0300

    Update main files
    Now working with the improved modules

--- sketch.js
+++ sketch.js
@@ -2,17 +2,11 @@ let ui;
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(24);
-  
-  ui = new UI();
-  
-  background(220);
-  textAlign(CENTER);
-  textSize(16);
-  stroke(100)
-  
-  text("Carregando...", width/2, height/2);
-  
+  frameRate(30);
+
+  ui = new UI("Banco de dados");
+  ui.renderBootScreen("Carregando");
+
   await ui.initLinks();
 }
 
